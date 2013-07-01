a1bbb55f653ae2210da78612e9f7a0a959a668c7
Hannes Sachsenhofer <Hannes@sachsenhofer.com>
2013-07-01 16:09:22 +0200

    Added example for creating a daily task that runs at midnight.

--- Examples/Create-Task.ps1
+++ Examples/Create-Task.ps1
@@ -1,5 +1,6 @@
 #include dependencies directly from GitHub
-iex (New-Object System.Net.WebClient).DownloadString("https://raw.github.com/saxx/ePunkt.PowerShell-Utilities/master/Create-Windows-Task.ps1")
+iex (New-Object System.Net.WebClient).DownloadString("https://raw.github.com/saxx/ePunkt.PowerShell-Utilities/master/Download-Dependencies.ps1")
 
 
 Create-Windows-Task-That-Runs-Every-15-Minutes "ePunkt eRecruiter CronWorker" "c:\eRecruiter\Bin\CronWorker\ePunkt.CronWorker.exe" "" "<THE_USERNAME>" "<THE_PASSWORD>"
+Create-Windows-Task-That-Runs-Every-Midnight "Server Backup" "c:\Backup\Backup.bat" "" "<THE_USERNAME>" "<THE_PASSWORD>"
