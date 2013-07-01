1c17a0fdd1facc5dd88645385dd1f914608dcc91
Hannes Sachsenhofer <Hannes@sachsenhofer.com>
2013-07-01 15:48:35 +0200

    Download only the dependencies that are really necessary.

--- Examples/Create-Task.ps1
+++ Examples/Create-Task.ps1
@@ -1,5 +1,5 @@
 #include dependencies directly from GitHub
-iex (New-Object System.Net.WebClient).DownloadString("https://raw.github.com/saxx/ePunkt.PowerShell-Utilities/master/Download-Dependencies.ps1")
+iex (New-Object System.Net.WebClient).DownloadString("https://raw.github.com/saxx/ePunkt.PowerShell-Utilities/master/Create-Windows-Task.ps1")
 
 
 Create-Windows-Task-That-Runs-Every-15-Minutes "ePunkt eRecruiter CronWorker" "c:\eRecruiter\Bin\CronWorker\ePunkt.CronWorker.exe" "" "<THE_USERNAME>" "<THE_PASSWORD>"
