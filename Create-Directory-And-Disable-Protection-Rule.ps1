0f29ba39e0ec89b11a3ecb7d1fed4ce457bc0a9c
Michael Mittermair <mittermair.michael@gmx.at>
2013-07-02 08:27:32 +0200

    added function to create a folder and disable inheritance of rights

---
+++ Create-Directory-And-Disable-Protection-Rule.ps1
@@ -0,0 +1,13 @@
+﻿function Create-Directory-And-Disable-Protection-Rule($directoryPath)
+{
+    New-Item -Path $directoryPath -ItemType directory
+
+    # Bestehenden Rechte übernehmen
+    $ACL = Get-Acl $directoryPath
+
+    # Vererbung ausschalten
+    $ACL.SetAccessRuleProtection($True, $True)
+
+    # veränderte Acl zurückschreiben
+    Set-Acl $directoryPath $ACL
+}
